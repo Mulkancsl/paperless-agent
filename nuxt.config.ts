ab904fcacb6bd179c573f086d75695cd7841568f
Mulkan <mulkancsl95@gmail.com>
2025-05-03 18:18:41 +0800

    Setup github pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,8 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
       ]
-    }
+    },
+    baseURL: process.env.NUXT_APP_BASE_URL || '/paperless-agent/'
   },
   devtools: { enabled: true },
   plugins: [
